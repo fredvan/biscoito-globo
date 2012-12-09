2f438031ffd419ecce7d1a08005e5e080fb1c889
Fredvan <fredvan.fernandes@gmail.com>
2012-12-09 19:51:36 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiscoitoGlobo::Application.config.secret_token = 'e256f4e1b5fb4098a37edea28771fcd075cde6d663ef87231e2721dae2ec914832558aef4b4470a4123c57209add590ebb9e17f8a7719c9c6085c89af3845076'
